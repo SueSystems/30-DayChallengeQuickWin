3310a0a485f610d49e0e02191bae8bad8b52c7f7
Susan <susansewe@gmail.com>
2026-04-09 12:07:08 +0300

    I/O: Getting integer and giving sum

---
+++ _input_int.py
@@ -0,0 +1,12 @@
+"""Getting Numerical - Input
+Getting an Integer from user and giving the sum ouput
+"""
+import sys
+def main():
+    """Getting two integers and giving its sum"""
+    a = int(input("Enter a number: "))
+    b = int(input("Enter another number: "))
+    print(a + b)
+
+if __name__ == "__main__":
+    sys.exit(main())
