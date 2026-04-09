fdb1e3dfd2bd7f638e52eba8767ebab2f1b7c9bd
Susan <susansewe@gmail.com>
2026-04-09 18:31:12 +0300

    Repeat: Word repeated number of user input times

---
+++ _word_repeated.py
@@ -0,0 +1,17 @@
+"""Word Repeated
+Input word of using repeated number of input times
+"""
+
+import sys
+
+def main():
+    """Inputer word repeated number of input times"""
+    word = input("Enter a word: ")
+    count = int(input("Enter a number: "))
+
+    #result = word * count
+    print("{} repeated {} times".format(word, count))
+    #print("The Word {} repeated {} times is {}.".format(word, count, count * word))
+
+if __name__== "__main__":
+    sys.exit(main())
