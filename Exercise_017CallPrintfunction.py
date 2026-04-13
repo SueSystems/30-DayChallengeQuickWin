58dcf68edc91c2b4fa27e52a0766291d7686215e
Susan <susansewe@gmail.com>
2026-04-13 17:20:22 +0300

    Call Print: :Print function on a message

---
+++ Exercise_017CallPrintfunction.py
@@ -0,0 +1,17 @@
+"""Call Print Function on a Message"""
+
+import sys
+
+
+def main():
+    """
+    Call print() on a message and assigning it to a
+    variable:"""
+
+    v = print("Hello")
+    print(v)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
