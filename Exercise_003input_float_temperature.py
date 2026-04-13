928ccb988ae077fee565fe5186789d775bf14249
Susan <susansewe@gmail.com>
2026-04-13 17:45:43 +0300

    Change of Exercise Name: Template maintained

--- Exercise_003input_float_temperature.py
+++ Exercise_003input_float_temperature.py
@@ -5,10 +5,10 @@ import sys
 
 def main():
     """Getting Temperature in Celsius and giving in Fahrenheit"""
-    _temperature = float(input("Enter Temperature in Degree Celsius: "))
-    result = _temperature * 1.8 + 32
-    # F = C * 9/5 + 32
-    print(result)
+    C = float(input("Enter Temperature in Degree Celsius: "))
+    #F = C * 1.8 + 32
+    F = C * 9/5 + 32
+    print(F)
 
 if __name__ == "__main__":
     sys.exit(main())
