dbe74c222c1f0ca69d74ae8621c905684f0b7567
Susan <susansewe@gmail.com>
2026-04-13 21:48:34 +0300

    List: Created and appended 3 words

---
+++ Exercise_019Listsinput.py
@@ -0,0 +1,18 @@
+"""List - Words input"""
+
+import sys
+
+
+def main():
+    """Creates  a list and input 3 words from the user"""
+
+    my_list = list()
+    my_list.append("Not")
+    my_list.append("yet")
+    my_list.append("understood")
+    print(my_list)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
