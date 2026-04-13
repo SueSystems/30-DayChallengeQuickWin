e79372097d671f21890836dbb1ba5cdedf7a722f
Susan <susansewe@gmail.com>
2026-04-13 18:44:01 +0300

    Change of Exercise Name: Template maintained

--- Exercise_011minutestohoursfloat.py
+++ Exercise_011minutestohoursfloat.py
@@ -4,13 +4,14 @@ Floats: Getting user input of minutes to be converted into hour
 
 import sys
 
+
 def main():
     """Converts user input of minutes to Hours"""
     minutes = float(input("Enter  minutes: "))
     #hours = int(input("Enter a hour: "))
 
     print("You entered {} minutes which is {} hours.".format(minutes, minutes/60))
-
+    return 0
 
 
 if __name__ == "__main__":
