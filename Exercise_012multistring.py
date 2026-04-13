e81f8bab9087ce3370cdb5a57b8d9536a0ddd659
Susan <susansewe@gmail.com>
2026-04-13 18:46:37 +0300

    Change of Exercise Name: Template maintained

--- Exercise_012multistring.py
+++ Exercise_012multistring.py
@@ -4,11 +4,14 @@ Multiply input string 3 times
 
 import sys
 
+
 def main():
     """MultiplyString using *"""
     x = input("Input a phrase:  ")
     multiply_sentence =  x * 3
     print(multiply_sentence)
+    return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
