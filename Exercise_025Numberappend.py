59ff97ad262edfc8b7e6f2c2f594c6fe050f6e20
Susan <susansewe@gmail.com>
2026-04-16 16:17:11 +0300

    Append a given number: On a given number list

---
+++ Exercise_025Numberappend.py
@@ -0,0 +1,15 @@
+"""Appending number on list of Numbers"""
+
+import sys
+
+
+def main():
+    """Append number 4 on list of given Numbers"""
+    nums = [1, 2, 3]
+    nums.append(4)
+    print(nums)
+    return 0
+
+
+if __name__ =="__main__":
+    sys.exit(main())
